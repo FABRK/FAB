5f86d5b6f890cf11646154abd1d68d8cce589a05
Gautam Sampathkumar <gsampathkumar@gmail.com>
2019-06-05 21:36:29 +0530

    update truffle-config

--- truffle-config.js
+++ truffle-config.js
@@ -71,6 +71,15 @@ module.exports = {
       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+        },
+    
+    mainnet: {
+        provider: () => mainNetProvider,
+        network_id: 1,       // Ropsten's id
+        gas: 5500000,        // Ropsten has a lower block limit than mainnet
+        confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+        timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+        skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
 
     // Useful for private networks
