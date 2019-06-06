7179a67461b1275375fab30fa0ba8d6cf9decf56
Gautam Sampathkumar <gsampathkumar@gmail.com>
2019-06-06 20:38:50 +0530

    removed timelock deployment

--- migrations/2_fab_token_migration.js
+++ migrations/2_fab_token_migration.js
@@ -4,9 +4,10 @@ const TokenTimelock = artifacts.require("TokenTimelock")
 const ethGetBlock = web3.eth.getBlock
 
 module.exports = function (deployer, networks, accounts) {
-    deployer.deploy(FABToken, "FAB Token", "FAB", 18).then(function () {
-        return ethGetBlock('latest')
-    }).then(function (block) {
-        return deployer.deploy(TokenTimelock, FABToken.address, accounts[0], block.timestamp + 30 *30 * 24 * 180)
-    })
+    deployer.deploy(FABToken, "FAB Token", "FAB", 18)
+    // .then(function () {
+    //     return ethGetBlock('latest')
+    // }).then(function (block) {
+    //     return deployer.deploy(TokenTimelock, FABToken.address, accounts[0], block.timestamp + 30 *30 * 24 * 180)
+    // })
 }
