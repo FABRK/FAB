5845552b402a48ddecd8e4bcca713eb31e66c522
Gautam Sampathkumar <gsampathkumar@gmail.com>
2019-06-06 16:27:56 +0530

    minor fixes

--- contracts/FABToken.sol
+++ contracts/FABToken.sol
@@ -5,14 +5,11 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 /**
  * @title ERC20 Token for the FAB Tokensale.
- * Is a Standard, Pausable and Burnable ERC20 token
- * Contains additional logic to register FAB addresses
- * and claim tokens for the FAB Mainnet swap.
+ * Is a Standard ERC20 token
  */
 contract FABToken is ERC20, ERC20Detailed{
   constructor(string memory _name, string memory _symbol, uint8 _decimals)
     ERC20Detailed(_name, _symbol, _decimals)
-    ERC20()
     public
   {
     _mint(msg.sender, 55000000000000000000000000000);
